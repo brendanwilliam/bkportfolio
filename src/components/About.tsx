0e054cb5143b9e842c73b9fc615491fa80c34dba
Brendan Keane <brendanwillkeane@gmail.com>
2024-05-22 10:56:49 -0700

    Updated resume file

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -19,9 +19,11 @@ function Biography() {
     <SubSection title="Biography">
       <div className="font-light text-neutral-600 text-sm space-y-1">
         <p>Trained in both UI/UX design and software engineering, Brendan&apos;s technical edge lets him develop digital products while being mindful technical limitations.<br/>
-        After starting his career as a UI/UX designer, Brendan transitioned to software engineering to better understand the developer experience when implementing his designs. From this experience he saw first-hand the challenges developers face at design handoff.<br/>
-        At Rough Draft Media, his unique skills shine in brand design, web development, video editing, and technical art. He also explores the intersection of design and technology as a game developer and UI artist.<br/>
-        Combining his passion for teaching technology and his experience in the field, Brendan is the lead designer for Smash Technology, a Seattle-based startup that provides tech industry training for underrepresented communities.<br/>
+        He started his career in UI/UX design, crafting experiences for web, mobile, and VR. His experience designing for multiple platforms shaped his skills in user research, wireframing, prototyping, and usability testing building a foundation for crafting responsive design systems in Figma.<br/>
+        Bothered by the disconnect between design details and developer implementation, Brendan transitioned into software engineering to better understand the developer experience. This practice introduced him to React, Next.js, and Tailwind CSS establishing analogues between Figma features and front-end developer best practices. He is currently developing a course titled <strong>Figma to Frontend</strong> to help prospective designers use technical constraints to simplify handoffs in cross-functional teams.<br/>
+        At Rough Draft Media, his unique skills shine in brand design, web development, video editing, and technical art where he produces and co-hosts podcasts and develops custom websites. In between projects, Brendan also explores the intersection of design and technology as a game developer and UI artist.<br/>
+        Combining his passion for teaching technology and his experience in the field, Brendan is the lead designer for Smash Technology, a Seattle-based startup that provides tech industry training for underrepresented communities. He is responsible for the course sales experience, student onboarding, and chatbot integration.<br/>
+        To learn more about Brendan, visit his LinkedIn profile or download his resume.
         </p>
       </div>
     </SubSection>
